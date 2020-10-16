381b936a8f8010a5b6e543f2f7f54d99fe231008
TimoLehnertz <timolehnertz1@gmail.com>
2020-10-16 03:16:05 +0200

    Auto Commit :=)

--- public_html/header.php
+++ public_html/header.php
@@ -12,6 +12,7 @@
         <link rel="stylesheet" href="/css/ui.css">
         <link rel="stylesheet" href="/css/normalize.css">
         <script src="/js/jquery-3.5.1.js"></script>
+        <script src="/js/jquery.mobile-1.4.5.min.js"></script>
         <script src="/js/ui.js"></script>
         <script src="https://kit.fontawesome.com/bb5d468397.js" crossorigin="anonymous"></script>
         <?php
