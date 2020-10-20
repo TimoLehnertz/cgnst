5a7673a38bf5c78e3d254e386e3a3d400bf635cc
TimoLehnertz <timolehnertz1@gmail.com>
2020-10-20 18:43:53 +0200

    Auto Commit :=)

--- public_html/js/title.js
+++ public_html/js/title.js
@@ -1,9 +1,22 @@
 $(function(){
-    $(window).on("scroll", ()=>{scroll(window.scrollY)});
-    scroll(window.scrollY);
+    window.addEventListener("scroll", throttle(scroll, 10));
+    scroll();
 });
 
-function scroll(scrollY){
-    $(".title-img").css("top", -scrollY / 2);
-    $("h1").css("top", -scrollY / 3 + 100);
+function scroll(){
+    $(".title-img").css("top", -window.scrollY / 2);
+    $("h1").css("top", -window.scrollY / 3 + 100);
 }
+
+function throttle (callback, limit) {
+    var wait = false;                 // Initially, we're not waiting
+    return function () {              // We return a throttled function
+        if (!wait) {                  // If we're not waiting
+            callback.call();          // Execute users function
+            wait = true;              // Prevent future invocations
+            setTimeout(function () {  // After a period of time
+                wait = false;         // And allow future invocations
+            }, limit);
+        }
+    }
+ }
